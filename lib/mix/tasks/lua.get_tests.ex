0a917ebca3f44bd7ccc57de70c4237df20801e48
Dave Lucia <dave@dave-mac-mini.tail23398.ts.net>
2026-02-10 14:17:03 -0800

    fix: Add Mix to dialyzer PLT instead of suppressing warnings
    Better solution: add :mix to plt_add_apps so dialyzer has type information
    for Mix module functions. Removes need for @dialyzer suppressions.

--- lib/mix/tasks/lua.get_tests.ex
+++ lib/mix/tasks/lua.get_tests.ex
@@ -22,10 +22,6 @@ defmodule Mix.Tasks.Lua.GetTests do
 
   use Mix.Task
 
-  # Suppress dialyzer warnings for Mix module functions (compile-time only)
-  @dialyzer {:nowarn_function,
-             run: 1, download_file: 2, extract_tarball: 2, remove_unnecessary_files: 1}
-
   @shortdoc "Downloads the Lua 5.3 test suite"
   @default_version "5.3.4"
   @base_url "https://www.lua.org/tests"
