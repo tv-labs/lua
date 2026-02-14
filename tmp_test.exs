b106a4510d4ab75c078e66c0ff57daa5da3b3e4b
Dave Lucia <dave@dave-mac-mini.tail23398.ts.net>
2026-02-13 18:49:04 -0800

    chore: Remove tmp test file

--- tmp_test.exs
+++
@@ -1,8 +0,0 @@
-source = File.read!("test/lua53_tests/constructs.lua")
-lines = String.split(source, "\n")
-
-chunk = Enum.take(lines, 96) |> Enum.join("\n")
-
-lua = Lua.new(exclude: [[:package], [:require]])
-{_, _lua} = Lua.eval!(lua, chunk)
-IO.puts("OK")
